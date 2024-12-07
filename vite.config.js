429af471db1e336fc760b6a7b02cad2cadde0efa
comeaubert <comeaubert@gmail.com>
2024-12-07 16:11:27 +0100

    Déploiment try n°3

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
-  base: '/SeasonStay/',
+  base: './',
   plugins: [react()],
-  server: {
-    port: 5173,
-    host: true,
-    strictPort: true,
-  },
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name].[hash][extname]',
-        chunkFileNames: 'assets/[name].[hash].js',
-        entryFileNames: 'assets/[name].[hash].js',
-      }
-    }
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  optimizeDeps: {
-    include: ['react', 'react-dom', 'react-router-dom'],
+    emptyOutDir: true,
   }
 })
