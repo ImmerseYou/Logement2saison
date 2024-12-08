2f5c8881fde366ebed6895b5b8892096cd3b2f5c
comeaubert <comeaubert@gmail.com>
2024-12-08 10:03:35 +0100

    fix: update vite config to use relative paths

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/SeasonStay/',
+  base: './',
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true
   }
 })
