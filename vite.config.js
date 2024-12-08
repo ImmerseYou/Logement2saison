e01261e84e1932aa52af1e45757b3a21005910ec
comeaubert <comeaubert@gmail.com>
2024-12-08 09:52:57 +0100

    déploiement action

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,18 @@ import react from '@vitejs/plugin-react'
 const isProduction = process.env.NODE_ENV === 'production'
 
 export default defineConfig({
-  base: isProduction ? '/SeasonStay/' : '/',
+  base: isProduction ? '/SeasonStay/' : '/SeasonStay/',
   plugins: [react()],
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[hash].[ext]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js',
+      },
+    },
   }
 })
