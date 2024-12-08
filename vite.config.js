6383939cde387e8027f52763ddaa1af2d8a15826
comeaubert <comeaubert@gmail.com>
2024-12-08 09:56:52 +0100

    action déploitement

--- vite.config.js
+++ vite.config.js
@@ -5,17 +5,29 @@ import react from '@vitejs/plugin-react'
 const isProduction = process.env.NODE_ENV === 'production'
 
 export default defineConfig({
-  base: isProduction ? '/SeasonStay/' : '/SeasonStay/',
+  base: '/SeasonStay/',
   plugins: [react()],
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
     emptyOutDir: true,
     rollupOptions: {
       output: {
-        assetFileNames: 'assets/[name].[hash].[ext]',
-        chunkFileNames: 'assets/[name].[hash].js',
-        entryFileNames: 'assets/[name].[hash].js',
+        assetFileNames: (assetInfo) => {
+          const info = assetInfo.name.split('.')
+          const ext = info[info.length - 1]
+          if (/\.(png|jpe?g|gif|svg|webp|ico)$/.test(assetInfo.name)) {
+            return `assets/images/[name].[hash][extname]`
+          }
+          if (/\.(css)$/.test(assetInfo.name)) {
+            return `assets/css/[name].[hash][extname]`
+          }
+          if (/\.(woff|woff2|eot|ttf|otf)$/.test(assetInfo.name)) {
+            return `assets/fonts/[name].[hash][extname]`
+          }
+          return `assets/[name].[hash][extname]`
+        },
+        chunkFileNames: 'assets/js/[name].[hash].js',
+        entryFileNames: 'assets/js/[name].[hash].js',
       },
     },
   }
